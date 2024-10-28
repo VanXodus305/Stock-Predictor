835fe7ae5d7180d2f842a15b293b5567ee2612e5
VanXodus305 <soumyadeep.30505@gmail.com>
2024-10-28 23:56:00 +0530

    Installed Next UI and code cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        foreground: "#083C70",
+        background: "#041D37",
+      },
+    },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 };
